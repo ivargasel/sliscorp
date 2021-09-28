8bb493060d7cd5387346465f81c12c0d7564e254
ivargasel <ivan.vargas@varelicr.com>
2021-09-28 17:41:46 -0600

    Partes del template

---
+++ en/Views/parts/foot.php
@@ -0,0 +1,12 @@
+<footer class="row bg-gray">
+    <div class="push-xxl-1 col-xxl-10 push-xl-1 col-xl-10 push-md-1 col-md-10 col-sm-12- col-xs-12">
+        <div class="col-xxl-6 col-xl-6 col-md-6 col-sm-12 col-xs-12">
+            Copyright © SLIS Corp <?php echo (DATE('Y')) ?>. All rights reserved.<br>
+            21011 Johnson Street Suite 110 Pembroke Pines, Florida
+        </div>
+        <div class="col-xxl-6 col-xl-6 col-md-6 col-sm-12 col-xs-12 right">
+            <a href="../es/index.php?action=Home">Español</a> | 
+            <a href="../en/index.php?action=Home">English</a>
+        </div>        
+    </div>    
+</footer>
---
+++ en/Views/parts/head.php
@@ -0,0 +1,15 @@
+<header class="row">
+    <div class="push-xxl-1 col-xxl-10 push-xl-1 col-xl-10 push-lg-1 col-lg-10 push-md-1 col-md-10 col-sm-12 col-xs-12">
+        <div class="col-xxl-1 col-xl-1 col-lg-1 col-md-1 col-sm-2 col-xs-3">
+            <a href="index.php?action=Home"><img src="https://sliscorp.com/img/SL-sin fondo.png" alt="SL Innovation and Solutions"></a>
+        </div>
+        <div class="col-xxl-11 col-xl-11 col-lg-11 col-md-11 col-sm-10 col-xs-9 right d-center">
+            <ul class="navbar">
+                <li class="navbar-item"><a href="index.php?action=Home">HOME</a></li>
+                <li class="navbar-item"><a href="index.php?action=Fintech">FINTECH SOLUTIONS</a></li>
+                <li class="navbar-item"><a href="index.php?action=Blockchain">BLOCKCHAIN</a></li>
+                <li class="navbar-item"><a href="index.php?action=Contact">CONTACT US</a></li>
+            </ul>
+        </div>                 
+    </div>    
+</header>
